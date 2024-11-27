b4259313d3dec369d0b87113f25d758fe8d0a831
yuragms <yura.gms@gmail.com>
2024-11-27 17:10:57 +0200

    03-create-dashboard-page

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,13 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function capitalizeFirstLetter(text: string) {
+  if (typeof text !== 'string') {
+    return '';
+  }
+  return text.charAt(0).toUpperCase() + text.slice(1);
 }
