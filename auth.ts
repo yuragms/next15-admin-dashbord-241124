6a63930ae79b5f59a5b9482b97a86ec3c7e8a198
yuragms <yura.gms@gmail.com>
2024-12-03 18:47:43 +0200

    08-authenticate-user-from-database

--- auth.ts
+++ auth.ts
@@ -2,19 +2,35 @@ import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
 import credentials from 'next-auth/providers/credentials';
 import { compare } from 'bcryptjs';
-import { users } from './lib/placeholder-data';
+// import { users } from './lib/placeholder-data';
+import { getUser } from './lib/actions/user.actions';
+import { z } from 'zod';
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
   providers: [
     credentials({
       async authorize(credentials) {
-        const user = users.find((x) => x.email === credentials.email);
-        if (!user) return null;
-        const passwordsMatch = await compare(
-          credentials.password as string,
-          user.password
-        );
-        if (passwordsMatch) return user;
+        // const user = users.find((x) => x.email === credentials.email);
+        // if (!user) return null;
+        // const passwordsMatch = await compare(
+        //   credentials.password as string,
+        //   user.password
+        // );
+        // if (passwordsMatch) return user;
+        const parsedCredentials = z
+          .object({
+            email: z.string().email(),
+            password: z.string().min(6),
+          })
+          .safeParse(credentials);
+
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const user = await getUser(email);
+          if (!user) return null;
+          const passwordsMatch = await compare(password, user.password);
+          if (passwordsMatch) return user;
+        }
         console.log('Invalid credentials');
         return null;
       },
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,5 +1,8 @@
 'use server';
 import { signIn } from '@/auth';
+import db from '@/db/drizzle';
+import { users241124 } from '@/db/schema';
+import { eq } from 'drizzle-orm';
 import { AuthError } from 'next-auth';
 export async function authenticate(
   prevState: string | undefined,
@@ -18,4 +21,12 @@ export async function authenticate(
     }
     throw error;
   }
+}
+
+export async function getUser(email: string) {
+  const user = await db.query.users241124.findFirst({
+    where: eq(users241124.email, email as string),
+  });
+  if (!user) throw new Error('User not found');
+  return user;
 }
