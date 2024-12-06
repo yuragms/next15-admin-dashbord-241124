c643af4b94c6a12eaa34aa8ec688b82b1b2862ae
yuragms <yura.gms@gmail.com>
2024-12-06 10:20:04 +0200

    need-fix

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -21,3 +21,31 @@ export default async function Page({
     </main>
   );
 }
+
+// import CustomersTable from '@/components/shared/customers/table';
+// import { fetchFilteredCustomers } from '@/lib/actions/customer.actions';
+// import { Metadata } from 'next';
+
+// export const metadata: Metadata = {
+//   title: 'Customers',
+// };
+
+// export default async function Page({
+//   searchParams,
+// }: {
+//   searchParams?: {
+//     query?: string;
+//     page?: string;
+//   };
+// }) {
+//   const query = searchParams?.query || '';
+//   const currentPage = Number(searchParams?.page) || 1;
+
+//   const customers = await fetchFilteredCustomers(query, currentPage);
+
+//   return (
+//     <main>
+//       <CustomersTable customers={customers} />
+//     </main>
+//   );
+// }
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,49 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+// import globals from "globals";
+// import pluginJs from "@eslint/js";
+// import tseslint from "typescript-eslint";
+// import pluginReact from "eslint-plugin-react";
 
+// /** @type {import('eslint').Linter.Config[]} */
+// export default [
+//   {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+//   {languageOptions: { globals: globals.browser }},
+//   pluginJs.configs.recommended,
+//   ...tseslint.configs.recommended,
+//   pluginReact.configs.flat.recommended,
+// ];
+
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import eslintConfigNext from 'eslint-config-next';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  eslintConfigNext.configs.recommended,
+  {
+    rules: {
+      // Настройте правила под ваши предпочтения
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'react/prop-types': 'off',
+    },
+  },
 ];
